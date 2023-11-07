4c99b26d91ffbdb70544a860ea77c973ba2b4378
Aayush Agarwal <crazy_nuclei@localhost>
2023-11-07 15:19:21 +0530

    Redirecting to Welcome page after login

--- src/main/java/com/buildStuff/myFirstWebApp/login/loginController.java
+++ src/main/java/com/buildStuff/myFirstWebApp/login/loginController.java
@@ -1,7 +1,10 @@
 package com.buildStuff.myFirstWebApp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class loginController {
@@ -9,5 +12,11 @@ public class loginController {
 	@RequestMapping("login")
 	public String getLoginPage() {
 		return "login";
+	}
+	
+	@RequestMapping(path = "login", method = RequestMethod.POST)
+	public String getWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+		model.put("name", name);
+		return "welcome";
 	}
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,14 @@
+<html> 
+	<head>
+		<title> Welcome Page </title>
+	</head>
+	
+	<body>
+		Welcome to Todo App, ${name } 
+		
+		<br><br>
+		
+		
+	</body>
+	
+</html>
